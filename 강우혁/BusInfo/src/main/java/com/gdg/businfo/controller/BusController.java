e0867c24a734098d13beea90c453361e869d9862
woohyuk0428 <whkang03@gmail.com>
2025-11-02 02:48:41 +0900

    feat: postman 추가 및 오류 수정

--- 강우혁/BusInfo/src/main/java/com/gdg/businfo/controller/BusController.java
+++ 강우혁/BusInfo/src/main/java/com/gdg/businfo/controller/BusController.java
@@ -4,7 +4,6 @@ import com.gdg.businfo.dto.BusInfoResponseDto;
 import com.gdg.businfo.dto.BusSaveRequestDto;
 import com.gdg.businfo.service.BusService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
